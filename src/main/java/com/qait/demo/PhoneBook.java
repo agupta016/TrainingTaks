c84069dbd93099fa83a0fea60fe813e651eca761
ayushi016 <iamayushi016@gmail.com>
2020-04-14 15:39:33 +0530

    commit

--- src/main/java/com/qait/demo/PhoneBook.java
+++ src/main/java/com/qait/demo/PhoneBook.java
@@ -4,29 +4,38 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import com.google.common.collect.Iterators;
+import java.util.Scanner;
 
 public class PhoneBook {
 	
 	HashMap<String,Integer> person = new HashMap<String, Integer>();
-	Iterator hmIterator  = person.entrySet().iterator();
+	
+	Scanner sr = new Scanner(System.in);
 	 void set() {
 		
-		
-		person.put("ayushi", 1234578);
-		person.put("gupta", 1234578);
-		person.put("abc", 1234578);
-		person.put("xyz", 1234578);
-		person.put("asdfgh", 1234578);
+		System.out.println("enter no. of users");
+		int n = sr.nextInt();
+		for(int i =0;i<n;i++)
+		{
+			System.out.println("enter user name and phone no.");
+			String name = sr.next();
+			int phone_no=sr.nextInt();
+			person.put(name, phone_no);
+			
+		}
 		
 	}
-	void get(String name) {
+	void get() {
+		
+		String name;
+		System.out.println("user name you want to search");
+		name = sr.next();
 		boolean flag=false;		
+		Iterator hmIterator  = person.entrySet().iterator();
 		while(hmIterator.hasNext())
 		{
 			Map.Entry element = (Entry) hmIterator.next();
-			if(element.getKey()==name)
+			if(((String) element.getKey()).equalsIgnoreCase(name))
 			{
 				flag =true;
 			  System.out.println(element.getKey() +"--"+element.getValue());
@@ -35,13 +44,15 @@ public class PhoneBook {
 		}
 		if(flag==false)
 			System.out.println("Person not registered in PhoneBook");
+		
+		
 	}
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PhoneBook pb = new PhoneBook();
 		pb.set();
-		pb.get("ayushim");
+		pb.get();
 
 	}
 
