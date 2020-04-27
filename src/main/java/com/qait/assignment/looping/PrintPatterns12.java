596a01d24b00d0a07694f827a7e153d1c6b2af68
ayushi016 <iamayushi016@gmail.com>
2020-04-27 14:51:49 +0530

    assignments on looping

--- src/main/java/com/qait/assignment/looping/PrintPatterns12.java
+++ src/main/java/com/qait/assignment/looping/PrintPatterns12.java
@@ -15,6 +15,42 @@ class PrintPatterns12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int n =8;
+		int k =1;
+		for(int i =1 ;i<=n;i++)
+		{
+			
+			for(int j=0;j<n;j++) {			
+					
+				if(k>9)
+					k=0;
+				
+				if(j>=n-i)
+				{
+					
+				   System.out.print(k+" ");
+				   k++;
+				   
+				}
+				else
+					System.out.print("  ");				
+			}
+			k--;
+			for(int j=1;j<i;j++) {			
+				
+				k--;	
+				if(k>9)
+					k=0;
+				if(k<0)
+					k=9;
+				System.out.print(k+" ");
+				
+						
+			}
+			k++;
+			System.out.println();
+		}
+		
 
 	}
 
--- src/main/java/com/qait/test/Test1.java
+++ src/main/java/com/qait/test/Test1.java
@@ -1,19 +1,18 @@
 package com.qait.test;
 
-import java.io.File;
 
-public class Test1  {
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1  {	
+		
 	
-	
-		 public static void main(String a[]){
-		        File file = new File("src/main/java/com/qait/test/Author.java");
-		            
-		        System.out.println(file.toURI());
-		        System.out.println(file.lastModified());
-		       
-		       }
-	 	    
+		
 	  }
+
+
 
 
 
