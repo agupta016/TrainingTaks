9445cab9445cf49513fc254116bd259bcd06f97a
ayushi016 <iamayushi016@gmail.com>
2020-05-01 16:25:14 +0530

    commit

--- src/main/java/com/qait/assignment/files/JsonFile.java
+++ src/main/java/com/qait/assignment/files/JsonFile.java
@@ -1,13 +1,10 @@
 package com.qait.assignment.files;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -19,14 +16,14 @@ public class JsonFile {
 	FileWriter fw;
 	JSONParser parser;
 	JSONObject obj;
-	JSONObject details;
+	JSONObject jsonfile;
 	
 	JsonFile() throws FileNotFoundException, IOException, ParseException{
 		
 	file = new File("SampleFiles/file.json");
 	parser = new JSONParser();
 	obj =(JSONObject)(parser.parse(new FileReader(file)));
-	details = (JSONObject)(obj.get("person"));
+	jsonfile = (JSONObject)(obj.get("person"));
 	fw = new FileWriter(file);
 		
 	}
@@ -41,17 +38,13 @@ public class JsonFile {
 	
 	public void readData() throws IOException, ParseException, org.json.simple.parser.ParseException {
 		
-//       BufferedReader br = new BufferedReader(new FileReader(file));		
-//		String str ;
-//		while((str = br.readLine())!=null)
-//			System.out.println(str);
 		
-		System.out.println(details);
+		System.out.println(jsonfile);
 		
-		JSONArray arr = (JSONArray) details.get("kids");
+		JSONArray arr = (JSONArray) jsonfile.get("kids");
 		System.out.println(arr);
 		
-		JSONArray hobby = (JSONArray) details.get("hobbies");
+		JSONArray hobby = (JSONArray) jsonfile.get("hobbies");
 		for(Object a:hobby)
 			System.out.println(a);
 		
@@ -63,12 +56,11 @@ public class JsonFile {
 		
 			
 		// add object in json object
-		
-		details.put("ID", "1");
-		details.put("Date_Of_Birth", "1989-09-26");
-		details.put("Place_Of_Birth", "Vishakhapatnam");
-		details.put("Country", "25000");
-		System.out.println(details);
+		jsonfile.put("name", "tinny George");
+		jsonfile.put("ID", "1");
+		jsonfile.put("Date_Of_Birth", "1989-09-26");
+		jsonfile.put("Country", "25000");
+		System.out.println(jsonfile);
 		
 		// add object into json array
 		JSONArray addKids = (JSONArray)(((JSONObject) obj.get("person")).get("kids"));
@@ -78,16 +70,22 @@ public class JsonFile {
 		addKids.add(kid);
 		System.out.println(addKids);
 		
-		System.out.println(details);
+		System.out.println(jsonfile);
 		
 	}
 	
 	public void removeObject() throws IOException {
-		details.remove("name");
-		details.remove("age");
-		System.out.println(details);
+		jsonfile.remove("name");
+		jsonfile.remove("age");
+		System.out.println(jsonfile);		
 		
-		fw.write(details.toJSONString());
+	}
+	
+	public void writeData() throws IOException {
+		
+		JSONObject ob = new JSONObject();
+		ob.put("person",jsonfile);
+		fw.write(ob.toJSONString());
 		fw.flush();
 		
 		
@@ -100,6 +98,7 @@ public class JsonFile {
 		pf.readData();		
 		pf.appendData();
 		pf.removeObject();
+		pf.writeData();
 		
 		
 		
--- src/main/java/com/qait/assignment/files/XmlFile.java
+++ src/main/java/com/qait/assignment/files/XmlFile.java
@@ -1,12 +1,29 @@
 package com.qait.assignment.files;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 
 public class XmlFile {
-	File file = new File("SampleFiles/file.properties");
+	static File file = new File("SampleFiles/file.xml");
 	
 	public void createFile() throws IOException {
 		
@@ -14,38 +31,91 @@ public class XmlFile {
 			file.createNewFile();		
 	}
 	
-	public void readData() throws IOException {
-		
-       BufferedReader br = new BufferedReader(new FileReader(file));		
-		String str ;
-		while((str = br.readLine())!=null)
-			System.out.println(str);
-		
-	} 
-	public void updateData() {
-		
-	}
-	
-	public void removeData() {
-		
-	}
-	
-	public void appendData() {
-		
-	}
-	
-	
-	public static void main(String[] args) throws IOException {
-		
-		PropertyFile pf = new PropertyFile();
-		pf.createFile();
-		pf.readData();
-		pf.updateData();
-		pf.removeData();
-		pf.appendData();
-		
-		
-		
-	}
+	public static void main(String[] args) {
+       DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
 
+            // parse xml file and load into document
+            Document doc = dBuilder.parse(file);
+
+            doc.getDocumentElement().normalize();
+
+            // update Element value
+            updateElementValue(doc);
+
+            // delete element
+            deleteElement(doc);
+
+            // add new element
+            addElement(doc);
+
+            // write the updated document to file or console
+            writeXMLFile(doc);
+
+        } catch (SAXException | ParserConfigurationException | IOException | TransformerException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    private static void writeXMLFile(Document doc)
+    throws TransformerFactoryConfigurationError, TransformerConfigurationException, TransformerException {
+        doc.getDocumentElement().normalize();
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(file);
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(source, result);
+        System.out.println("XML file updated successfully");
+    }
+
+    /**
+     * Add a new element salary to user element.
+     * @param doc
+     */
+    private static void addElement(Document doc) {
+        NodeList users = doc.getElementsByTagName("User");
+        Element emp = null;
+
+        // loop for each user
+        for (int i = 0; i < users.getLength(); i++) {
+            emp = (Element) users.item(i);
+            Element salaryElement = doc.createElement("salary");
+            salaryElement.appendChild(doc.createTextNode("10000"));
+            emp.appendChild(salaryElement);
+        }
+    }
+
+    /**
+     * Delete gender element from User element
+     * @param doc
+     */
+    private static void deleteElement(Document doc) {
+        NodeList users = doc.getElementsByTagName("User");
+        Element user = null;
+        // loop for each user
+        for (int i = 0; i < users.getLength(); i++) {
+            user = (Element) users.item(i);
+            Node genderNode = user.getElementsByTagName("gender").item(0);
+            user.removeChild(genderNode);
+        }
+
+    }
+
+    /**
+     * Update firstName element value to Upper case.
+     * @param doc
+     */
+    private static void updateElementValue(Document doc) {
+        NodeList users = doc.getElementsByTagName("User");
+        Element user = null;
+        // loop for each user
+        for (int i = 0; i < users.getLength(); i++) {
+            user = (Element) users.item(i);
+            Node name = user.getElementsByTagName("firstName").item(0).getFirstChild();
+            name.setNodeValue(name.getNodeValue().toUpperCase());
+        }
+    }
 }
